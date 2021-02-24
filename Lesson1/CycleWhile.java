b11343613e8b7f1584e3ab334361f59b2c99af3f
Khmyzenko Roman <neon1978@yandex.ru>
2021-02-24 10:43:35 +0300

    imliment HW-1

---
+++ Lesson1/CycleWhile.java
@@ -0,0 +1,12 @@
+public class CycleWhile{
+    
+    public static void main(String[] args){
+    
+        int value = -6;
+    
+        while (value<=6){
+            System.out.println(value);
+            value = value + 2;
+        }
+    }
+}
